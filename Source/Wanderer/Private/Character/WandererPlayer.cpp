fd8895893c62af0ec8572fcf719bd03b93474aa7
alexr <aarichason@csuchico.edu>
2023-12-24 10:36:27 -0800

    idk might be broke

--- Source/Wanderer/Private/Character/WandererPlayer.cpp
+++ Source/Wanderer/Private/Character/WandererPlayer.cpp
@@ -4,6 +4,7 @@
 #include "Character/WandererPlayer.h"
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
+#include "EnhancedInputComponent.h"
 
 AWandererPlayer::AWandererPlayer() {
   PlayerCamera = CreateDefaultSubobject<UCameraComponent>("PlayerCamera");
@@ -14,11 +15,31 @@ AWandererPlayer::AWandererPlayer() {
 
 }
 
+
+// Making changes here trying to use enhanced
+//void AWandererPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) {
+/*
+void AWandererPlayer::SetupPlayerInputComponent(UEnhancedInputComponent* PlayerInputComponent) {
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	//PlayerInputComponent->BindAxis("MoveForward", this, &AWandererPlayer::MoveForward);
+	//PlayerInputComponent->BindAxis("MoveSide", this, &AWandererPlayer::MoveSide);
+	PlayerInputComponent->BindAction(MoveForwardAction, ETriggerEvent::Triggered, this, &AWandererPlayer::MoveForward);
+}
+
+void AWandererPlayer::MoveForward(float Value) {
+	AddMovementInput(GetActorForwardVector(), Value);
+}
+
+*/
 void AWandererPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-	PlayerInputComponent->BindAxis("MoveForward", this, &AWandererPlayer::MoveForward);
-	PlayerInputComponent->BindAxis("MoveSide", this, &AWandererPlayer::MoveSide);
+	UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerInputComponent);
+	if (EnhancedInputComponent) {
+		EnhancedInputComponent->BindAction(MoveForwardAction, ETriggerEvent::Triggered, this, &AWandererPlayer::MoveForward);
+		EnhancedInputComponent->BindAction(MoveSideAction, ETriggerEvent::Triggered, this, &AWandererPlayer::MoveSide);
+	}
 }
 
 void AWandererPlayer::MoveForward(float Value) {
--- Source/Wanderer/Public/Character/WandererPlayer.h
+++ Source/Wanderer/Public/Character/WandererPlayer.h
@@ -4,6 +4,9 @@
 #include "CoreMinimal.h"
 #include "Character/WandererCharacterBase.h"
 #include "WandererPlayer.generated.h"
+#include "EnhancedInputComponent.h"
+#include "InputMappingContext.h"
+#include "InputAction.h"
 
 class UCameraComponent;
 class USpringArmComponent;
@@ -20,16 +23,25 @@ public:
 	AWandererPlayer();
 
 protected:
+	//virtual void SetupPlayerInputComponent(class UInputComponent* SetupPlayerInputComponent) override;
+	virtual void SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) override;
+
+	void MoveForward(float Value);
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	UCameraComponent* PlayerCamera;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	USpringArmComponent* SpringArm;
 
-	virtual void SetupPlayerInputComponent(class UInputComponent* SetupPlayerInputComponent) override;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UInputMappingContext* InputMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UInputAction* MoveForwardAction;
 
 private:
-	void MoveForward(float Value);
-	void MoveSide(float Value);
+	//void MoveForward(float Value);
+	//void MoveSide(float Value);
 
 };
